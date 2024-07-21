86046753b75daaa8c0a805d911cb5e6ae53a8afe
Zixuan Lu <luzixuan@gmail.com>
2024-07-21 15:36:41 -0700

    use js to import nav

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 // importnav bar htmml
-// const nav = document.querySelector('.navbar')
-// fetch('navbar.html')
-//     .then(res => res.text())
-//     .then(data => {
-//         nav.innerHTML = data
-//         const parser = new DOMParser()
-//         const doc = parser.parseFromString(data, 'text/html')
-//         eval(doc.querySelector('script').textContent)
-//     })
+const nav = document.querySelector('.navbar')
+fetch('navbar.html')
+    .then(res => res.text())
+    .then(data => {
+        nav.innerHTML = data
+        const parser = new DOMParser()
+        const doc = parser.parseFromString(data, 'text/html')
+        eval(doc.querySelector('script').textContent)
+    })
 
 
 // reveal words through griaffe spots (about me page)
